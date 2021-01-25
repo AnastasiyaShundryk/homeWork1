d8deedcb5bfed834942e2950df19fe80a48277be
Anastasiya_Shundryk <anastasiya_shundryk@epam.com>
2021-01-25 17:53:24 +0300

    2

--- src/test/java/page/Task3.java
+++ src/test/java/page/Task3.java
@@ -1,4 +1,13 @@
 package page;
 
+import org.openqa.selenium.WebDriver;
+
 public class Task3 {
+    private String URL="https://cloud.google.com/";
+    private WebDriver driver;
+    private String locatorSearchField="//devsite-search/form/div[1]/div/input";
+    private String locatorSearchBtn= "//*[text()=All results for \"google cloud platform pricing calculator\"]";
+    private String textSearch="Google Cloud Platform Pricing Calculator";
+
+    private String locatorSearchResult="//b[text()='Google Cloud Platform Pricing Calculator']";
 }
